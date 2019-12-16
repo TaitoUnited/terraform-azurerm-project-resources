75cf8ed9acaeebddd60be1b854e4f4c6682d7dba
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-12-16 23:11:13 +0200

    feat: implementation

---
+++ storage.tf
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2019 Taito United
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "azurerm_storage_account" "project" {
+  count                    = length(var.storages) > 0 ? 1 : 0
+
+  name                     = replace("${var.project}-${var.env}", "-", "")
+  resource_group_name      = data.azurerm_resource_group.namespace.name
+  location                 = data.azurerm_resource_group.namespace.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    project = var.project
+    env     = var.env
+    purpose = "storage"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_storage_container" "bucket" {
+  count                 = length(var.storages)
+  name                  = var.storages[count.index]
+  storage_account_name  = azurerm_storage_account.project[0].name
+  container_access_type = "private"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
