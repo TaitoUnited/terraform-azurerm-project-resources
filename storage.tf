fce83dbaed759f1310cceaa0fcaa07f4768eee8b
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-10-20 12:53:57 +0300

    feat: add storage delete_retention_policy

--- storage.tf
+++ storage.tf
@@ -57,14 +57,12 @@ resource "azurerm_storage_account" "account" {
       }
     }
 
-    /* TODO: enable once GA
     dynamic "delete_retention_policy" {
       for_each = each.value.versioningRetainDays != null ? [ each.value.versioningRetainDays ] : []
       content {
         days = delete_retention_policy.value
       }
     }
-    */
 
     dynamic "container_delete_retention_policy" {
       for_each = each.value.versioningRetainDays != null ? [ each.value.versioningRetainDays ] : []
